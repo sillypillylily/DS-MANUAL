6a66e5e5d99193107623cd5a849c228b4b034450
Srishti Singh <srishti134340@gmail.com>
2026-09-18 10:22:54 +0530

    Add files via upload

---
+++ 1b.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, j, key;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Insertion Sort
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        // Move elements greater than key one position ahead
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        // Insert key at its correct position
+        arr[j + 1] = key;
+    }
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
